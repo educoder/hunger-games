0c2b26b71644b42bcaec55e5c3df93bbbb539b90
Armin Krauss <armin.krauss@utoronto.ca>
2013-09-19 11:52:05 -0400

    Refactoring of var names

--- js/roster_to_rollcall.js
+++ js/roster_to_rollcall.js
@@ -7,41 +7,40 @@
   // instantiate rollcall so I can read and write the users
   copycat.rollcall = new Rollcall('http://drowsy.badger.encorelab.org', 'rollcall');
 
-  copycat.copy_hard = function() {
+  copycat.copyHard = function() {
     jQuery.when(jQuery.ajax('http://ltg.evl.uic.edu:9292/roster/people'), copycat.rollcall.users())
-      .done(function (run_roster, rollcall_users) {
-        console.log(run_roster);
-        console.log(rollcall_users);
+      .done(function (runRoster, rollcallUsers) {
+        console.log(runRoster);
+        console.log(rollcallUsers);
 
-        _.each(run_roster[0], function(uic_user) {
+        _.each(runRoster[0], function(uicUser) {
           // has roster user a run that matches defined runs?
-          if (_.contains(runs, uic_user.run)) {
-            var userId = uic_user._id.substring(4,7);
-            var result = rollcall_users.findWhere({username: userId});
+          if (_.contains(runs, uicUser.run)) {
+            var userId = uicUser._id.substring(4,7);
+            var result = rollcallUsers.findWhere({username: userId});
             if (result) {
               console.log('Update user: '+result.get('username'));
               var tags = result.get('tags');
-              tags.push(uic_user.run);
-              tags.push(uic_user.run);
+              tags.push(uicUser.run);
               result.set('tags', _.uniq(tags));
               // result.save();
             } else {
               console.log('user not found - create');
               var newUser = new copycat.rollcall.User({
                 username: userId,
-                tags: [uic_user.run],
-                rfid_tag: uic_user.rfid_tag,
-                color: uic_user.color,
-                color_label: uic_user.color_label
+                tags: [uicUser.run],
+                rfid_tag: uicUser.rfid_tag,
+                color: uicUser.color,
+                color_label: uicUser.color_label
               });
               // newUser.save();
-              rollcall_users.add(newUser);
+              rollcallUsers.add(newUser);
               console.log('Created user with username: '+userId);
             }
           }
         });
 
-        rollcall_users.each(function (u) {
+        rollcallUsers.each(function (u) {
           u.save();
         });
       });
